1d5853991ec00fa3e1d3870a172acb3a40b2d458
cggg88jorge <jorge@makingdevs.com>
2018-04-09 16:56:49 -0500

    #5 Ajustando el nombre del servicio del tomcat

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 echo "Ejecuntando script"
 service nginx start
-service tomcat-qa start
-service tomcat-stage start
+service tomcat-web start
+service tomcat-api start
 tail -F -n0 /etc/hosts
 echo "Terminanod script"
